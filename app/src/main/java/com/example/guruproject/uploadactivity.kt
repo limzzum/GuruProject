02606bf63f7ceb611f8e2157894785076c5b46f2
limzzum <chfhddl1234@naver.com>
2020-08-15 21:35:33 +0900

    추가

--- app/src/main/java/com/example/guruproject/uploadactivity.kt
+++ app/src/main/java/com/example/guruproject/uploadactivity.kt
@@ -3,13 +3,14 @@ package com.example.guruproject
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_uploadactivity.*
 
 class uploadactivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_upload)
+        setContentView(R.layout.activity_uploadactivity)
         upload_button.setOnClickListener {
-            val ToUploadView= Intent(this@UploadActivity,Uploadview::class.java)
+            val ToUploadView= Intent(this@uploadactivity,Uploadview::class.java)
             startActivity(ToUploadView)
         }
     }
