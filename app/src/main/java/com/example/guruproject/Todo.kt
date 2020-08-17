4efb00a7dc72e03372b137bdc99a95427b86c8ed
KimMinJeong05 <min980512@gmail.com>
2020-08-17 17:35:37 +0900

    minjeong 8/17 data 데이터 형태로 추가하고 싶은 데이터 추가해주세요

---
+++ app/src/main/java/com/example/guruproject/Todo.kt
@@ -0,0 +1,19 @@
+package com.example.guruproject
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+
+data class Todo(val category: String, val content: String)
+
+
+//fun addTodo(){
+//    //val todo = Todo(binding.editText.text.toString())
+//    //data.add(todo)
+//    //binding.recyclerView.adapter.notifyDataSetChanged()
+//}
+//fun deleteTodo(){
+//
+//}
