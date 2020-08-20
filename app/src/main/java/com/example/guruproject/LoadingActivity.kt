8ba28d3ce065137c8017c61f87138ce2dda05eb4
김예원 <victory2588@naver.com>
2020-08-20 21:04:09 +0900

    yewon

--- app/src/main/java/com/example/guruproject/LoadingActivity.kt
+++ app/src/main/java/com/example/guruproject/LoadingActivity.kt
@@ -1,11 +1,19 @@
 package com.example.guruproject
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 
 class LoadingActivity : AppCompatActivity() {
+    val SPLASH_VIEW_TIME: Long = 2000
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_loading)
+
+        Handler().postDelayed({
+            startActivity(Intent(this, TreeActivity::class.java))
+            finish()
+        }, SPLASH_VIEW_TIME)
     }
 }
