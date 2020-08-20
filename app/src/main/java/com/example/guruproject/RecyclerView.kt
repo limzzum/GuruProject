c0d3b751f9b93a7f919ae1bb91a891498dfd0755
limzzum <chfhddl1234@naver.com>
2020-08-20 14:06:23 +0900

    Merge remote-tracking branch 'origin/master' into master
    # Conflicts:
    #	app/src/main/AndroidManifest.xml

--- app/src/main/java/com/example/guruproject/RecyclerView.kt
+++ app/src/main/java/com/example/guruproject/RecyclerView.kt
@@ -72,14 +72,40 @@ class RecyclerView : AppCompatActivity() {
                     //profiles.add(Profiles(userId=UserId, username = Username,postContent =  PostContent))
                     // profiles.add(profilelist)
                 }
-
+            }
                 //profiles.add(Profiles(UserId,Username,PostContent))
 
                 //   profiles=document.toObject(Profiles::class.java)
                 //profileList.add(profiles)
 
             }
-    }
+
+    override fun onStart() {
+        super.onStart()
+
+        var ProfileListDTO: ArrayList<Profiles> = ArrayList<Profiles>()
+            var db = FirebaseFirestore.getInstance()
+
+
+            db.collection("users").whereEqualTo("userId", "User")
+                .get()
+                .addOnSuccessListener { result ->
+
+                    var profileDTO: Profiles
+
+                    for (document in result) {
+                        //Log.d(TAG,"$document.id}=>$document.data}")
+                        profileDTO = document.toObject(Profiles::class.java)
+                        ProfileListDTO.add(profileDTO)
+
+                        recycler_view.layoutManager =
+                            LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
+                        recycler_view.setHasFixedSize(true)
+                        recycler_view.adapter = ProfileAdapter(ProfileListDTO)
+                    }
+                }
+
+    }}
 
 //        }
 
